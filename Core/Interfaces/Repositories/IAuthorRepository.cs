af762a5334fd35c3c717fc169b08465634e8afaa
adebisi4145 <adeyemiadebisi00@yahoo.com>
2021-10-04 14:25:52 +0100

    Author And Category

---
+++ Core/Interfaces/Repositories/IAuthorRepository.cs
@@ -0,0 +1,21 @@
+﻿using Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Interfaces.Repositories
+{
+    public interface IAuthorRepository
+    {
+        public Author GetAuthor(int id);
+        public IList<Author> GetAuthors();
+
+        public Author UpdateAuthor(Author author);
+
+        public Author AddAuthor(Author author);
+
+        public void DeleteAuthor(int id);
+    }
+}
---
+++ Library.Domain/Services/CategoryService.cs
@@ -0,0 +1,68 @@
+﻿using Core.DTOs;
+using Core.Interfaces.Repositories;
+using Core.Interfaces.Services;
+using Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Library.Domain.Services
+{
+    public class CategoryService : ICategoryService
+    {
+        private readonly ICategoryRepository _categoryRepository;
+
+        public CategoryService(ICategoryRepository categoryRepository)
+        {
+            _categoryRepository = categoryRepository;
+        }
+        public BaseResponse AddCategory(CreateCategoryRequestModel model)
+        {
+            var category = new Category
+            {
+                Name = model.Name
+            };
+            _categoryRepository.AddCategory(category);
+            return new BaseResponse
+            {
+                Status = true,
+                Message = "Successfully Added"
+            };
+        }
+
+        public void DeleteCategory(int id)
+        {
+            _categoryRepository.DeleteCategory(id);
+        }
+
+        public IList<Category> GetCategories()
+        {
+            return _categoryRepository.GetCategories();
+        }
+
+        public CategoryModel GetCategory(int id)
+        {
+            var category = _categoryRepository.GetCategory(id);
+
+            return new CategoryModel
+            {
+                Name = category.Name,
+            };
+        }
+
+        public BaseResponse UpdateCategory(int id, UpdateCategoryRequestModel model)
+        {
+            var category = _categoryRepository.GetCategory(id);
+            category.Name = model.Name;
+            _categoryRepository.UpdateCategory(category);
+
+            return new BaseResponse
+            {
+                Status = true,
+                Message = "Successfully Updated"
+             };
+        }
+    }
+}
